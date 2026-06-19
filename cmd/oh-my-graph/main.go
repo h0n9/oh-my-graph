33ed96d42ed5b79f13b7a38340268d89a95b1d72
Jaehong Ahn <monsieur.ahn@gmail.com>
2026-06-19 20:49:18 +0900

    feat: add CLI entry point with --port and --data flags

---
+++ cmd/oh-my-graph/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+	"time"
+
+	"github.com/h0n9/oh-my-graph/internal/graph"
+	"github.com/h0n9/oh-my-graph/internal/mcp"
+)
+
+func main() {
+	port := flag.Int("port", 8080, "HTTP listen port")
+	data := flag.String("data", "", "data directory (default: ~/.oh-my-graph)")
+	flag.Parse()
+
+	dir := resolveDir(*data)
+	mgr := graph.NewManager(dir)
+	srv := mcp.NewServer(mgr)
+
+	httpSrv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", *port),
+		Handler: srv,
+	}
+
+	go func() {
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+		<-sig
+
+		log.Println("oh-my-graph: shutting down...")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		httpSrv.Shutdown(ctx)
+		mgr.Close()
+	}()
+
+	log.Printf("oh-my-graph: listening on :%d, data at %s", *port, dir)
+	if err := httpSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("oh-my-graph: %v", err)
+	}
+}
+
+func resolveDir(data string) string {
+	if data != "" {
+		return data
+	}
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return ".oh-my-graph"
+	}
+	return filepath.Join(home, ".oh-my-graph")
+}
